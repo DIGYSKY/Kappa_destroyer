0ba628ca0d0deb20031a6a5838f3b5356de6a7c9
DIGYSKY <lilyan.chauveau@gmail.com>
2023-12-04 13:46:03 +0100

    Add in object kappa methode : levelUp. And waepon damage

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 let kappa = {
     codaPoint: 0,
+    level: {curent: 0, scoreUpReach: {
+        1: 1000, 2: 5000, 3: 15000,
+        4: 30000, 5: 50000, 6: 75000, 
+        7: 100000
+    }, damage: {
+        1: 250, 2: 400, 3: 500,
+        4: 500, 5: 500, 6: 500,
+        7: null
+    }},
+    weapon: {
+        damage: 100,
+    },
+    levelUp: function () {
+        this.level.curent += 1;
+        this.weapon.damage = this.level.damage[this.level.curent];
+    },
     scoreUp: function (up) {
         this.codaPoint += up;
     },
@@ -10,15 +26,3 @@ let kappa = {
         return this.codaPoint;
     }
 }
-
-let opponent = {
-
-}
-
-kappa.scoreUp(45);
-
-console.log(kappa.returnCodaPoint());
-
-kappa.reset();
-
-console.log(kappa.returnCodaPoint());
